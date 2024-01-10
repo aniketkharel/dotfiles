b8ca876b2100dc2cc64b16d5deff6c7d29fe0bfc
aniketkharel <aniketkhareldev@gmail.com>
2024-01-10 22:25:59 +1100

    nvchad config

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,25 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = { 
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "arrow",
+    overriden_modules = nil,
+  },
+
+  tabufline = {
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  -- ...other options
+  theme = 'rosepine'
+}
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
