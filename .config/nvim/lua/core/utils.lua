2e5f63938b3d528ea4cc66464685ad67f3a7ac8f
aniketkharel <aniketkhareldev@gmail.com>
2024-01-13 08:30:15 +1100

    back to old configs

--- .config/nvim/lua/core/utils.lua
+++ .config/nvim/lua/core/utils.lua
@@ -1,118 +1,70 @@
-local M = {}
-local merge_tb = vim.tbl_deep_extend
+local options = {
+	background = "dark",
+	backup = false,
+	clipboard = "unnamedplus",
+	cmdheight = 1,
+	completeopt = { "menuone", "noselect" },
+	conceallevel = 2,
+	fileencoding = "utf-8",
+	spelllang = "en_us",
+	--hlsearch = true,
+	incsearch = true,
+	ignorecase = true,
+	mouse = "a",
+	pumheight = 10,
+	showmode = false,
+	showtabline = 1,
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	termguicolors = true,
+	timeoutlen = 1000,
+	undofile = true,
+	updatetime = 300,
+	writebackup = false,
+	expandtab = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	cursorline = false,
+	cursorcolumn = false,
+	colorcolumn = "80",
+	number = true,
+	relativenumber = true,
+	numberwidth = 2,
+	signcolumn = "yes",
+	wrap = false,
+	scrolloff = 10,
+	laststatus = 3,
+	sidescrolloff = 10,
+	breakindent = true,
+	showbreak = string.rep(" ", 3),
+	linebreak = true,
+	foldmethod = "marker",
+	foldlevel = 0,
+	modelines = 1,
+}
 
-M.load_config = function()
-  local config = require "core.default_config"
-  local chadrc_path = vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1]
+vim.opt.shortmess:append("c")
 
-  if chadrc_path then
-    local chadrc = dofile(chadrc_path)
-
-    config.mappings = M.remove_disabled_keys(chadrc.mappings, config.mappings)
-    config = merge_tb("force", config, chadrc)
-    config.mappings.disabled = nil
-  end
-
-  return config
+for k, v in pairs(options) do
+	vim.opt[k] = v
 end
 
-M.remove_disabled_keys = function(chadrc_mappings, default_mappings)
-  if not chadrc_mappings then
-    return default_mappings
-  end
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
 
-  -- store keys in a array with true value to compare
-  local keys_to_disable = {}
-  for _, mappings in pairs(chadrc_mappings) do
-    for mode, section_keys in pairs(mappings) do
-      if not keys_to_disable[mode] then
-        keys_to_disable[mode] = {}
-      end
-      section_keys = (type(section_keys) == "table" and section_keys) or {}
-      for k, _ in pairs(section_keys) do
-        keys_to_disable[mode][k] = true
-      end
-    end
-  end
+vim.cmd("set nohidden")
 
-  -- make a copy as we need to modify default_mappings
-  for section_name, section_mappings in pairs(default_mappings) do
-    for mode, mode_mappings in pairs(section_mappings) do
-      mode_mappings = (type(mode_mappings) == "table" and mode_mappings) or {}
-      for k, _ in pairs(mode_mappings) do
-        -- if key if found then remove from default_mappings
-        if keys_to_disable[mode] and keys_to_disable[mode][k] then
-          default_mappings[section_name][mode][k] = nil
-        end
-      end
-    end
-  end
+-- netrw configs
+vim.g.netrw_banner = 0
+vim.opt.spell = false
 
-  return default_mappings
-end
+vim.opt.foldmethod = "manual"
+vim.opt.foldenable = false
 
-M.load_mappings = function(section, mapping_opt)
-  vim.schedule(function()
-    local function set_section_map(section_values)
-      if section_values.plugin then
-        return
-      end
-
-      section_values.plugin = nil
-
-      for mode, mode_values in pairs(section_values) do
-        local default_opts = merge_tb("force", { mode = mode }, mapping_opt or {})
-        for keybind, mapping_info in pairs(mode_values) do
-          -- merge default + user opts
-          local opts = merge_tb("force", default_opts, mapping_info.opts or {})
-
-          mapping_info.opts, opts.mode = nil, nil
-          opts.desc = mapping_info[2]
-
-          vim.keymap.set(mode, keybind, mapping_info[1], opts)
-        end
-      end
-    end
-
-    local mappings = require("core.utils").load_config().mappings
-
-    if type(section) == "string" then
-      mappings[section]["plugin"] = nil
-      mappings = { mappings[section] }
-    end
-
-    for _, sect in pairs(mappings) do
-      set_section_map(sect)
-    end
-  end)
-end
-
-M.lazy_load = function(plugin)
-  vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile" }, {
-    group = vim.api.nvim_create_augroup("BeLazyOnFileOpen" .. plugin, {}),
-    callback = function()
-      local file = vim.fn.expand "%"
-      local condition = file ~= "NvimTree_1" and file ~= "[lazy]" and file ~= ""
-
-      if condition then
-        vim.api.nvim_del_augroup_by_name("BeLazyOnFileOpen" .. plugin)
-
-        -- dont defer for treesitter as it will show slow highlighting
-        -- This deferring only happens only when we do "nvim filename"
-        if plugin ~= "nvim-treesitter" then
-          vim.schedule(function()
-            require("lazy").load { plugins = plugin }
-
-            if plugin == "nvim-lspconfig" then
-              vim.cmd "silent! do FileType"
-            end
-          end, 0)
-        else
-          require("lazy").load { plugins = plugin }
-        end
-      end
-    end,
-  })
-end
-
-return M
+-- ripgrep setup
+vim.cmd([[set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case]])
+vim.cmd([[set grepformat=%f:%l:%c:%m]])
+vim.cmd([[set spell]])
---
+++ .config/nvim/lua/lsp/lspsaga.lua
@@ -0,0 +1,35 @@
+local status, saga = pcall(require, "lspsaga")
+if not status then
+  return
+end
+
+saga.setup {
+  server_filetype_map = {
+    typescript = "typescript",
+  },
+  symbol_in_winbar = {
+    enable = true,
+    separator = "-",
+    ignore_patterns = {},
+    hide_keyword = true,
+    show_file = true,
+    folder_level = 2,
+    respect_root = false,
+    color_mode = true,
+  },
+  lightbulb = {
+    enable = false,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+}
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+vim.keymap.set("i", "<C-k>", "<Cmd>Lspsaga signature_help<CR>", opts)
+vim.keymap.set("n", "gp", "<Cmd>Lspsaga preview_definition<CR>", opts)
+vim.keymap.set("n", "<leader>r", "<Cmd>Lspsaga rename<CR>", opts)
