7903f88e42e4f60f41874b5cd2a04869d0c17c30
aniketkharel <aniketkhareldev@gmail.com>
2024-01-14 21:38:17 +1100

    transparent neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,12 @@ require("plugins")
 -- lsp setup
 require("lsp")
 
+-- Set background color of "Normal" text in GUI to none
+vim.cmd("highlight Normal guibg=none")
+-- Set background color of "NonText" elements in GUI to none
+vim.cmd("highlight NonText guibg=none")
+-- Set background color of "Normal" text in terminal to none
+vim.cmd("highlight Normal ctermbg=none")
+-- Set background color of "NonText" elements in terminal to none
+vim.cmd("highlight NonText ctermbg=none")
+
