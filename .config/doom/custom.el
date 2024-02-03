3e98bc08fdcd731d7e30728a1dd046bb1f139f3d
aniketkharel <aniketkhareldev@gmail.com>
2024-02-04 09:17:02 +1100

    i3 dual screen setup

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("636b135e4b7c86ac41375da39ade929e2bd6439de8901f53f88fde7dd5ac3561" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
    '("/home/aniketdev/org/agendas/todos.org" "/home/aniketdev/org/agendas/work.org" "/home/aniketdev/org/agendas/home.org"))
  '(org-directory "~/org")
