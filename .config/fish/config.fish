ffee86ebd85afa9b22b47069bbac1bd6dfeec762
aniketkharel <aniketkhareldev@gmail.com>
2024-06-10 12:49:33 +1000

    fish conf and i3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -103,13 +103,6 @@ alias org "cd $HOME/org && rgf"
 if status is-interactive
 end
 
-# pnpm
-set -gx PNPM_HOME "/home/aniketdev/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
